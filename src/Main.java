98964aa78ea03bb2a939135e6eade255b49a88c8
AbdelatefElshafei <abdelatefmohammedali@gmail.com>
2024-11-27 22:01:18 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,3 @@
-import javax.xml.crypto.Data;
-import java.sql.Time;
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Patient p1 = new Patient(23101421, "Tefa", 18, "+201025799528", "ADHD");
-        Date d1 = new Date(12,2);
-        Date.Time t1 = new Date.Time(5,30);
 public class Main {
     public static void main(String[] args) {
 
