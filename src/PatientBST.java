65168c30f92e494637bc86a13d4fe3a833ac34a1
AbdelatefElshafei <abdelatefmohammedali@gmail.com>
2024-11-28 17:22:04 +0200

    Building BST

---
+++ src/PatientBST.java
@@ -0,0 +1,12 @@
+public class PatientBST {
+    public class Node {
+        private Patient patient ;
+        private Node left,right;
+        public Node(Patient p){
+            this.patient = p; 
+            left = right = null;
+        }
+    }
+    Node root ; 
+    public
+}
